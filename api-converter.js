ef94c0337bbe18747b6b6542adfeaf8e41b3d30e
unknown <martsalazar0@gmail.com>
2021-11-01 19:13:08 +0800

    common word collection json key

--- api-converter.js
+++ api-converter.js
@@ -39,20 +39,24 @@ const convertEndpoints = file => {
     // Filters data that includes the common word
     const filterLines = (lines, commonWord) => {
         filteredData = lines.filter(line => line.includes(commonWord))
+        
         console.log({filteredData})
         
         // Create array of objects with endpoint and request method fields
         const jsonContent = filteredData.map(data => {
             const method = data.includes('get') || data.includes('fetch') ? 'GET' : (data.includes('update') ? 'PUT' : (data.includes('delete') ? 'DELETE' : 'POST'))
-            return {
-                endpoint: data.split(' = ')[1],
-                method
-            }
+            return [ data.split(' = ')[1], method]
         })
 
-        fs.writeFile('filteredEndpoints.json', JSON.stringify(jsonContent), (e)=>{
+        const formattedJSON = {}
+        formattedJSON[`${commonWord} Collection`] = jsonContent
+
+        fs.writeFile(`${commonWord}ApiCollection.json`, JSON.stringify(formattedJSON), (e)=>{
             if(e) console.log(e)
-            else console.log('JSON File created Successfully')
+            else {
+                console.log('JSON File created Successfully')
+                console.log(formattedJSON)
+            }
         })
     }
 }
